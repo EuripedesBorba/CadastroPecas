9ce093f259c65fd6a59cfe0e1153e93b7863a24a
EuripedesJacob <euripedeseuquero@gmail.com>
2021-10-26 16:45:23 -0300

    organizando pasta

---
+++ contador.js
@@ -0,0 +1,21 @@
+
+/// Exerciocio encontro remoto 2 
+
+/*Ok - Saber o número total de alunos
+Percorrer de 0 até o número total de alunos
+          Se o número for 0, escrever “ZERO” na tela
+ 	Se o número for impar, escrever ......
+                 Se o número for par, escrever ......*/
+
+// 1)
+
+ let numeroDeAlunos = 5
+Console.log(numeroDeAlunos)
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+// 2) laço contado
+
+
+ //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+
+
